21aa8f01b873e35193c34ac2d6fab9775a3be6f7
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-21 19:32:32 +0100

    add babel-middleware config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
-module.exports = {
+const config = {
   port: 80,
   host: 'localhost',
+  babel: {
+    srcPath: 'js',
+    cachePath: __dirname + '/_cache',
+    babelOptions: {
+      presets: ['es2015', 'stage-0'],
+    },
+  },
 };
