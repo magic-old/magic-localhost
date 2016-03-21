f6ce92151ee82083f6f68246d850a644e016f89f
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-21 15:40:05 +0100

    remove load.js, is in layout.jade now

--- src/public/js/load.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var d = document
-  , s = d.createElement('script')
-  , l = d.getElementById('load-js')
-;
-s.src = '/js/main.js';
-d.body.appendChild(s);
-if (l && l.parentNode) l.parentNode.removeChild(l);
