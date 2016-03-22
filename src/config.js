c88a0ea89ab6d84e8f011a4b95764820f4da5cb7
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-22 17:11:50 +0100

    update babelify options and html js src

--- src/config.js
+++ src/config.js
@@ -1,11 +1,7 @@
 const config = {
   port: 80,
   host: 'localhost',
-  babel: {
-    srcPath: 'js',
-    cachePath: __dirname + '/_cache',
-    babelOptions: {
-      presets: ['es2015', 'stage-0'],
-    },
-  },
+  babelifyFiles: [
+    'bundle',
+  ],
 };
